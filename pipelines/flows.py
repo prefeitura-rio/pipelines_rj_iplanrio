335bc79a98b44c93d423af50ecf0d4fdf384603d
d116626 <d116626@gmail.com>
2024-04-18 11:02:43 -0300

    feat: add dbt flow

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -4,3 +4,4 @@ Imports all flows for every project so we can register all of them.
 """
 from pipelines.processorio import *  # noqa
 from pipelines.sicop import *  # noqa
+from pipelines.templates import *  # noqa

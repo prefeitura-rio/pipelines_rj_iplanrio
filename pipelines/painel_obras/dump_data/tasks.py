30482c252b979530f8d336514700d49211453067
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2024-05-22 08:51:19 -0300

    fix: show errors

--- pipelines/painel_obras/dump_data/tasks.py
+++ pipelines/painel_obras/dump_data/tasks.py
@@ -52,6 +52,8 @@ def download_data_to_gcs(  # pylint: disable=R0912,R0913,R0914,R0915
     job = bq_client.query(query)
     while not job.done():
         sleep(1)
+    if job.errors:
+        raise ValueError(f"Error while querying data: {job.errors}")
     dest_table = job._properties["configuration"]["query"]["destinationTable"]
     dest_project_id = dest_table["projectId"]
     dest_dataset_id = dest_table["datasetId"]

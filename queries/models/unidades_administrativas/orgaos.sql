d6df61ef3994276f734675c3b9a44ee23af22228
Bruno Almeida <brunodepauloalmeida@gmail.com>
2024-07-16 12:30:53 -0300

    fix: query with duplicate columns

--- queries/models/unidades_administrativas/orgaos.sql
+++ queries/models/unidades_administrativas/orgaos.sql
@@ -1,29 +1,26 @@
 SELECT 
   SAFE_CAST(
     REGEXP_REPLACE(cd_ua, r'\.0$', '') AS STRING
-  ) id_unidade_administrativa,
+  ) as id_unidade_administrativa,
   SAFE_CAST(
     REGEXP_REPLACE(sigla_ua, r'\.0$', '') AS STRING
-  ) sigla_unidade_administrativa,
+  ) as sigla_unidade_administrativa,
   SAFE_CAST(
     REGEXP_REPLACE(nome_ua, r'\.0$', '') AS STRING
-  ) nome_unidade_administrativa,
+  ) as nome_unidade_administrativa,
   SAFE_CAST(
     REGEXP_REPLACE(cd_ua_pai, r'\.0$', '') AS STRING
-  ) id_unidade_administrativa_pai,
-  SAFE_CAST(
-    REGEXP_REPLACE(cd_ua_pai, r'\.0$', '') AS STRING
-  ) id_unidade_administrativa_pai,
+  ) as id_unidade_administrativa_pai,
   SAFE_CAST(
     REGEXP_REPLACE(nivel, r'\.0$', '') AS STRING
-  ) nivel,
+  ) as nivel,
   SAFE_CAST(
     REGEXP_REPLACE(ordem_ua_basica, r'\.0$', '') AS STRING
-  ) ordem_unidade_administrativa_basica,
+  ) as ordem_unidade_administrativa_basica,
   SAFE_CAST(
     REGEXP_REPLACE(ordem_absoluta, r'\.0$', '') AS STRING
-  ) ordem_absoluta,
+  ) as ordem_absoluta,
   SAFE_CAST(
     REGEXP_REPLACE(ordem_relativa, r'\.0$', '') AS STRING
-  ) ordem_relativa
+  ) as ordem_relativa
 FROM `rj-iplanrio.unidades_administrativas_staging.orgaos`

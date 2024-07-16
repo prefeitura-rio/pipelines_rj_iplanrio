c6a70a358c3a92054fc466128f297145508c4f5f
Bruno Almeida <brunodepauloalmeida@gmail.com>
2024-07-16 12:30:34 -0300

    fix: schedule to daily

--- pipelines/sici/dump_api/schedules.py
+++ pipelines/sici/dump_api/schedules.py
@@ -8,13 +8,11 @@ from prefect.schedules.clocks import IntervalClock
 
 from pipelines.constants import constants
 
-TIMEDELTA_ONCE_A_MONTH = timedelta(minutes=43830)
-
 sici_dump_api_schedule = Schedule(
     clocks=[
         IntervalClock(
-            interval=TIMEDELTA_ONCE_A_MONTH,
-            start_date=datetime(2024, 8, 12, 18, tzinfo=pytz.timezone("America/Sao_Paulo")),
+            interval=timedelta(days=1),
+            start_date=datetime(2024, 7, 17, 18, tzinfo=pytz.timezone("America/Sao_Paulo")),
             labels=[
                 constants.RJ_IPLANRIO_AGENT_LABEL.value,
             ],

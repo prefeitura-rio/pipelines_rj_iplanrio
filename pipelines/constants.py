b39d75440a8e0dd3b2de64f1a4e7560588718ba0
Pedro Mendes <pedrohrmendes@proton.me>
2024-09-13 10:21:56 -0300

    feat: register taxi_rio flows

--- pipelines/constants.py
+++ pipelines/constants.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from enum import Enum
 
 
@@ -7,20 +6,11 @@ class constants(Enum):
     # Automatically managed,
     # please do not change these values
     ######################################
-    # Docker image
     DOCKER_TAG = "AUTO_REPLACE_DOCKER_TAG"
     DOCKER_IMAGE_NAME = "AUTO_REPLACE_DOCKER_IMAGE"
     DOCKER_IMAGE = f"{DOCKER_IMAGE_NAME}:{DOCKER_TAG}"
     GCS_FLOWS_BUCKET = "datario-public"
+    ######################################
 
-    ######################################
-    # Agent labels
-    ######################################
-    # EXAMPLE_AGENT_LABEL = "example_agent"
     RJ_IPLANRIO_AGENT_LABEL = "iplanrio"
-
-    ######################################
-    # Other constants
-    ######################################
-    # EXAMPLE_CONSTANT = "example_constant"
     SICI_SOAP_API_WSDL = "http://sici.rio.rj.gov.br/Servico/WebServiceSICI.asmx?wsdl"
--- pipelines/flows.py
+++ pipelines/flows.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Imports all flows for every project so we can register all of them.
-"""
+"""Imports all flows for every project so we can register all of them."""
+
 # from pipelines.processorio import *  # noqa
 from pipelines.painel_obras import *  # noqa
 from pipelines.sicop import *  # noqa
 from pipelines.templates import *  # noqa
 from pipelines.sici import *  # noqa
+from pipelines.taxi_rio import *

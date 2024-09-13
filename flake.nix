bea9766bb9329d97cd93a99c629fdf9512977b90
Pedro Mendes <pedrohrmendes@proton.me>
2024-09-13 10:23:35 -0300

    project: add nix flakes support

--- flake.nix
+++ flake.nix
@@ -18,17 +18,22 @@
           config.allowUnfree = true;
         };
       in {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            infisical
-            poetry
-            python310
-          ];
+        devShells.default = with pkgs;
+          mkShell {
+            packages = [
+              infisical
+              python310
+              (poetry.override {python3 = python310;})
+            ];
 
-          shellHook = ''
-            source ./.venv/bin/activate
-          '';
-        };
+            shellHook = ''
+              source ./.venv/bin/activate
+            '';
+
+            LD_LIBRARY_PATH = lib.makeLibraryPath [
+              stdenv.cc.cc
+            ];
+          };
       }
     );
 }

4445f321d5883adb5fdbc07df562a6cbb8b6c8db
Pedro Mendes <pedrohrmendes@proton.me>
2024-09-13 10:43:06 -0300

    refactor(taxirio): rename variables

--- pipelines/taxi_rio/constants.py
+++ pipelines/taxi_rio/constants.py
@@ -3,6 +3,6 @@ from enum import Enum
 
 class constants(Enum):
     TAXI_RIO_MONGODB_NAME = "taxirio"
-    RJ_TAXI_RIO_AGENT_LABEL = "iplanrio"
+    RJ_IPLANRIO_TAXIRIO_AGENT_LABEL = "iplanrio"
     DATASET_ID = "transporte_rodoviario_taxirio_staging"
     TABLE_ID = "cities"

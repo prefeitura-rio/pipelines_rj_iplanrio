f3886e56c265a994de58bf21b6ff5007deca23a9
Pedro Mendes <pedrohrmendes@proton.me>
2024-09-16 13:34:07 -0300

    refactor(pipelines/taxirio): create mongo instance as a default factory in a dataclass

--- pipelines/taxirio/utils.py
+++ pipelines/taxirio/utils.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from pymongo import MongoClient
 from pymongo.collection import Collection
@@ -7,12 +7,19 @@ from pipelines.taxirio.constants import constants
 from pipelines.utils import get_env_variable
 
 
+def _mongo_client() -> MongoClient:
+    """Get a MongoDB client."""
+    return MongoClient(get_env_variable("DB_CONNECTION_STRING"))
+
+
 @dataclass(frozen=True)
 class MongoTaxiRio:
-    client: MongoClient = MongoClient(get_env_variable("DB_CONNECTION_STRING"))
+    """Class to handle MongoDB connections for the taxirio project."""
+
+    client: MongoClient = field(default_factory=_mongo_client)
 
     def get_collection(self, collection: str) -> Collection:
-        """Get a MongoDB collection by name"""
+        """Get a MongoDB collection by name."""
         database = self.client[constants.TAXIRIO_MONGODB_NAME.value]
 
         return database[collection]

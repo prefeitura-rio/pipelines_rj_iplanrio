3123deade55fb964c8625cb478953d8519407347
Pedro Mendes <pedrohrmendes@proton.me>
2024-09-17 10:15:57 -0300

    refactor(pipelines/taxirio): rename functions and change their dependencies

--- pipelines/taxirio/cities/flows.py
+++ pipelines/taxirio/cities/flows.py
@@ -9,11 +9,20 @@ from pipelines.taxirio.constants import constants as local_constants
 with Flow(
     "IPLANRIO: cities - Dump da tabela do MongoDB do TaxiRio",
 ) as rj_iplanrio_taxirio_cities_flow:
-    connection_string = tasks.get_mongo_connection_string()
-    client = tasks.get_mongo_client(connection_string)
-    cities = tasks.get_cities_collection(client)
-    data = tasks.get_cities_data(cities)
+    connection = tasks.get_mongo_connection_string()
+
+    client = tasks.get_mongo_client(connection)
+
+    cities = tasks.get_mongo_collection(
+        client,
+        local_constants.RJ_IPLANRIO_TAXIRIO_AGENT_LABEL.value,
+        local_constants.TABLE_ID.value,
+    )
+
+    data = tasks.get_collection_data(cities)
+
     dataframe = tasks.convert_to_df(data)
+
     tasks.save_to_csv(dataframe)
 
 rj_iplanrio_taxirio_cities_flow.storage = GCS(global_constants.GCS_FLOWS_BUCKET.value)

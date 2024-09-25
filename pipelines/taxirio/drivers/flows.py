aec3a9a2b0975613baa1e25f826ace1d0ec12962
Pedro Mendes <pedrohrmendes@proton.me>
2024-09-25 15:51:23 -0300

    refactor(taxirio/drivers): import functions
    .

--- pipelines/taxirio/drivers/flows.py
+++ pipelines/taxirio/drivers/flows.py
@@ -4,37 +4,42 @@ from prefeitura_rio.pipelines_utils.custom import Flow
 from prefeitura_rio.pipelines_utils.state_handlers import handler_inject_bd_credentials
 
 from pipelines.constants import constants
-from pipelines.taxirio import tasks as taxirio_tasks
 from pipelines.taxirio.constants import Constants as TaxiRio
-from pipelines.taxirio.drivers import tasks as drivers_tasks
 from pipelines.taxirio.drivers.constants import Constants as Drivers
+from pipelines.taxirio.drivers.tasks import iterate_over_mongo_cursor
 from pipelines.taxirio.schedules import every_month
+from pipelines.taxirio.tasks import (
+    get_collection_data,
+    get_mongo_client,
+    get_mongo_collection,
+    get_mongo_connection_string,
+)
 
 with Flow(
     name="IPLANRIO: drivers - Dump da tabela do MongoDB do TaxiRio",
     state_handlers=[handler_inject_bd_credentials],
     skip_if_running=True,
     parallelism=10,
-) as rj_iplanrio__taxirio__paymentmethods__flow:
-    connection = taxirio_tasks.get_mongo_connection_string()
+) as rj_iplanrio__taxirio__drivers__flow:
+    connection = get_mongo_connection_string()
 
-    client = taxirio_tasks.get_mongo_client(connection)
+    client = get_mongo_client(connection)
 
-    drivers_collection = taxirio_tasks.get_mongo_collection(
+    drivers_collection = get_mongo_collection(
         client,
         TaxiRio.RJ_IPLANRIO_TAXIRIO_AGENT_LABEL.value,
         Drivers.TABLE_ID.value,
     )
 
-    data = taxirio_tasks.get_collection_data(drivers_collection, batch=1000)
+    data = get_collection_data(drivers_collection, batch=1000)
 
-    drivers_tasks.iterate_over_mongo_cursor(data)
+    iterate_over_mongo_cursor(data)
 
-rj_iplanrio__taxirio__paymentmethods__flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+rj_iplanrio__taxirio__drivers__flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 
-rj_iplanrio__taxirio__paymentmethods__flow.schedule = every_month
+rj_iplanrio__taxirio__drivers__flow.schedule = every_month
 
-rj_iplanrio__taxirio__paymentmethods__flow.run_config = KubernetesRun(
+rj_iplanrio__taxirio__drivers__flow.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value,
     labels=[TaxiRio.RJ_IPLANRIO_TAXIRIO_AGENT_LABEL.value],
 )
--- pipelines/taxirio/drivers/tasks.py
+++ pipelines/taxirio/drivers/tasks.py
@@ -2,18 +2,18 @@ from prefect import task
 from prefeitura_rio.pipelines_utils.tasks import create_table_and_upload_to_gcs
 from pymongo.cursor import Cursor
 
-from pipelines.taxirio import tasks
 from pipelines.taxirio.constants import Constants as TaxiRio
 from pipelines.taxirio.drivers.constants import Constants as Drivers
+from pipelines.taxirio.tasks import convert_to_df, save_to_csv
 
 
 @task
 def iterate_over_mongo_cursor(data: Cursor) -> None:
     """Iterate over a MongoDB cursor and save the data to a CSV file."""
     for count, chunk in enumerate(data):
-        dataframe = tasks.convert_to_df(chunk)
+        dataframe = convert_to_df(chunk)
 
-        path = tasks.save_to_csv(
+        path = save_to_csv(
             dataframe,
             f"{Drivers.TABLE_ID.value}_{count}",
         )

602d8ae229ed1b06190c2c09cacba8f907f6130d
Pedro Mendes <pedrohrmendes@proton.me>
2024-09-26 17:38:23 -0300

    fix(taxirio/drivers): change parallelism

--- pipelines/taxirio/drivers/flows.py
+++ pipelines/taxirio/drivers/flows.py
@@ -15,10 +15,10 @@ with Flow(
     name="IPLANRIO: drivers - Dump da tabela do MongoDB do TaxiRio",
     state_handlers=[handler_inject_bd_credentials],
     skip_if_running=True,
-    parallelism=10,
+    parallelism=1,
 ) as rj_iplanrio__taxirio__drivers__flow:
     create_table_and_upload_to_gcs.map(
-        data_path=dump_collection_from_mongodb_in_batches(Drivers.TABLE_ID.value),
+        data_path=dump_collection_from_mongodb_in_batches(Drivers.TABLE_ID.value, 2000),
         table_id=unmapped(Drivers.TABLE_ID.value),
         dataset_id=unmapped(TaxiRio.DATASET_ID.value),
         dump_mode=unmapped("append"),

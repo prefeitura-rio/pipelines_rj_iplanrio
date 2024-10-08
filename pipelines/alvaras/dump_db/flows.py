5f2dcac9f709f6ef9e52e15aa5d585f1ebde5160
PedroSiqueira1 <pedro.siqueira@poli.ufrj.br>
2024-10-08 16:11:33 -0300

    fix: Change to SQL Server

--- pipelines/alvaras/dump_db/flows.py
+++ pipelines/alvaras/dump_db/flows.py
@@ -35,9 +35,9 @@ rj_iplanrio_alvaras_flow.run_config = KubernetesRun(
 
 alvaras_default_parameters = {
     "db_database": "DW_BI_ALVARAS",
-    "db_host": "srv000144.infra.rio.gov.br",
+    "db_host": "srv000144.rio.gov.br",
     "db_port": "1433",
-    "db_type": "oracle",
+    "db_type": "sql_server",
     "dataset_id": "alvaras",
     "infisical_secret_path": "/db-alvaras",
 }

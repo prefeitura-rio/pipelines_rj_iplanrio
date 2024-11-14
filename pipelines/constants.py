64391e8e53153b0b31046740931988d3fa97b559
Pedro Mendes <pedrohrmendes@proton.me>
2024-11-14 14:21:28 -0300

    feat(taxirio/cities): add DBT flow

--- pipelines/constants.py
+++ pipelines/constants.py
@@ -14,6 +14,12 @@ class Constants(Enum):
     GCS_FLOWS_BUCKET = "datario-public"
     ######################################
 
+    FLOW_DUMP_TO_GCS_NAME = "IPLANRIO: template - Ingerir tabela zipada para GCS"
+    FLOW_EXECUTE_DBT_MODEL_NAME = "IPLANRIO: template - Executa DBT model"
+    MAX_BYTES_PROCESSED_PER_TABLE = 5 * 1024 * 1024 * 1024
+    PREFECT_DEFAULT_PROJECT = "main"
     RJ_IPLANRIO_AGENT_LABEL = "iplanrio"
     SICI_SOAP_API_WSDL = "http://sici.rio.rj.gov.br/Servico/WebServiceSICI.asmx?wsdl"
     TIMEZONE = "America/Sao_Paulo"
+    WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS = 3
+    WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL = 5
--- pipelines/taxirio/cities/flows.py
+++ pipelines/taxirio/cities/flows.py
@@ -1,9 +1,12 @@
-from prefect import Parameter
+from datetime import timedelta
+
+from prefect import Parameter, case
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
+from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
 from prefeitura_rio.pipelines_utils.custom import Flow
 from prefeitura_rio.pipelines_utils.state_handlers import handler_inject_bd_credentials
-from prefeitura_rio.pipelines_utils.tasks import create_table_and_upload_to_gcs
+from prefeitura_rio.pipelines_utils.tasks import create_table_and_upload_to_gcs, get_current_flow_labels
 
 from pipelines.constants import Constants
 from pipelines.taxirio.cities.constants import Constants as Cities
@@ -24,8 +27,18 @@ with Flow(
     parallelism=1,
 ) as rj_iplanrio__taxirio__cities__flow:
     path = Parameter("path", default="output")
-    dataset_id = Parameter("dataset_id", default=TaxiRio.DATASET_ID.value)
     secret_name = Parameter("secret_name", default=TaxiRio.MONGODB_CONNECTION_STRING.value)
+    dataset_id = Parameter("dataset_id", default=TaxiRio.DATASET_ID.value)
+    table_id = Parameter("table_id", default=Cities.TABLE_ID.value)
+    dump_to_gcs = Parameter("dump_to_gcs", default=False, required=False)
+    materialization_mode = Parameter("mode", default="dev", required=False)
+    materialize_after_dump = Parameter("materialize_after_dump", default=False, required=False)
+    materialize_to_datario = Parameter("materialize_to_datario", default=False, required=False)
+    maximum_bytes_processed = Parameter(
+        "maximum_bytes_processed",
+        required=False,
+        default=Constants.MAX_BYTES_PROCESSED_PER_TABLE.value,
+    )
 
     connection = get_mongodb_connection_string(secret_name)
 
@@ -34,7 +47,7 @@ with Flow(
     collection = get_mongodb_collection(
         client,
         TaxiRio.MONGODB_DATABASE_NAME.value,
-        Cities.TABLE_ID.value,
+        table_id,
     )
 
     data_path = dump_collection_from_mongodb(
@@ -44,13 +57,67 @@ with Flow(
         pipeline=pipeline,
     )
 
-    create_table_and_upload_to_gcs(
+    upload_table = create_table_and_upload_to_gcs(
         data_path=data_path,
         dataset_id=dataset_id,
         dump_mode="overwrite",
         source_format="parquet",
-        table_id=Cities.TABLE_ID.value,
+        table_id=table_id,
     )
+
+    with case(materialize_after_dump, True):
+        current_flow_labels = get_current_flow_labels()
+
+        materialization_flow = create_flow_run(
+            flow_name=Constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+            project_name=Constants.PREFECT_DEFAULT_PROJECT.value,
+            parameters={
+                "dataset_id": dataset_id,
+                "table_id": table_id,
+                "mode": materialization_mode,
+                "materialize_to_datario": materialize_to_datario,
+            },
+            labels=current_flow_labels,
+            run_name=f"Materialize {dataset_id}.{table_id}",
+        )
+
+        materialization_flow.set_upstream(upload_table)
+
+        wait_for_materialization = wait_for_flow_run(
+            materialization_flow,
+            stream_states=True,
+            stream_logs=True,
+            raise_final_state=True,
+        )
+
+        wait_for_materialization.max_retries = Constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+        wait_for_materialization.retry_delay = timedelta(
+            seconds=Constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
+        )
+
+    with case(dump_to_gcs, True):
+        dump_to_gcs_flow = create_flow_run(
+            flow_name=Constants.FLOW_DUMP_TO_GCS_NAME.value,
+            project_name=Constants.PREFECT_DEFAULT_PROJECT.value,
+            parameters={
+                "project_id": "datario",
+                "dataset_id": dataset_id,
+                "table_id": table_id,
+                "maximum_bytes_processed": maximum_bytes_processed,
+            },
+            labels=[
+                "datario",
+            ],
+            run_name=f"Dump to GCS {dataset_id}.{table_id}",
+        )
+        dump_to_gcs_flow.set_upstream(wait_for_materialization)
+
+        wait_for_dump_to_gcs = wait_for_flow_run(
+            dump_to_gcs_flow,
+            stream_states=True,
+            stream_logs=True,
+            raise_final_state=True,
+        )
 
 rj_iplanrio__taxirio__cities__flow.storage = GCS(Constants.GCS_FLOWS_BUCKET.value)
 

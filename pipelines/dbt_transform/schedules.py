afd80d6c707b48c08252001fb00c32dee1fde7ed
PedroSiqueira1 <pedro.siqueira@poli.ufrj.br>
2025-04-01 07:14:09 -0300

    fix: Update schedules to use production environment and remove unused parameters

--- pipelines/dbt_transform/schedules.py
+++ pipelines/dbt_transform/schedules.py
@@ -13,23 +13,14 @@ from pipelines.constants import Constants
 from pipelines.utils_dbt.schedules import generate_dump_api_schedules, untuple_clocks
 
 daily_parameters = [
-    {"command": "build", "environment": "dev", "rename_flow": True, "select": "tag:daily"},
-    {"command": "source freshness", "environment": "dev", "rename_flow": True},
+    {"command": "build", "environment": "prod", "rename_flow": True, "select": "tag:daily"},
+    {"command": "source freshness", "environment": "prod", "rename_flow": True},
 ]
 
 weekly_parameters = [
-    {"command": "build", "environment": "dev", "rename_flow": True, "select": "tag:weekly"},
+    {"command": "build", "environment": "prod", "rename_flow": True, "select": "tag:weekly"},
 ]
 
-every_30_minutes_parameters = [
-    {
-        "command": "build",
-        "environment": "dev",
-        "rename_flow": True,
-        "select": "tag:alerta_doencas",
-        "send_discord_report": False,
-    },
-]
 
 
 dbt_daily_clocks = generate_dump_api_schedules(
@@ -52,16 +43,6 @@ dbt_weekly_clocks = generate_dump_api_schedules(
     runs_interval_minutes=30,
 )
 
-dbt_every_30_minutes_clocks = generate_dump_api_schedules(
-    interval=timedelta(minutes=30),
-    start_date=datetime(2024, 12, 18, 10, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
-    labels=[
-        Constants.RJ_IPLANRIO_AGENT_LABEL.value,
-    ],
-    flow_run_parameters=every_30_minutes_parameters,
-    runs_interval_minutes=0,
-)
-
-dbt_clocks = dbt_daily_clocks + dbt_weekly_clocks + dbt_every_30_minutes_clocks
+dbt_clocks = dbt_daily_clocks + dbt_weekly_clocks 
 
 dbt_schedules = Schedule(clocks=untuple_clocks(dbt_clocks))

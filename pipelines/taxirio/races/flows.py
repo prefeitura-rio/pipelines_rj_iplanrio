61dbbee37a76dbfb0c08f9e6aafbf078445d4bea
Pedro Mendes <pedrohrmendes@proton.me>
2025-04-02 17:55:13 -0300

    refactor: update memory limits

--- pipelines/taxirio/races/flows.py
+++ pipelines/taxirio/races/flows.py
@@ -79,5 +79,5 @@ rj_iplanrio__taxirio__races__flow.run_config = KubernetesRun(
     image=Constants.DOCKER_IMAGE.value,
     labels=[TaxiRio.RJ_TAXIRIO_AGENT_LABEL.value],
     memory_request="1Gi",
-    memory_limit="5Gi",
+    memory_limit="10Gi",
 )

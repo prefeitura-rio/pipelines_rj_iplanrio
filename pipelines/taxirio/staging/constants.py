6e016eb8b65dad26a3e7347404f3a32a2f8f3d11
Pedro Mendes <phrmendes00@proton.me>
2025-04-03 15:29:08 -0300

    refactor(taxirio): lower memory limit and rename datasets (#48)
    * refactor: rename datasets
    
    * refactor: lower memory limit

--- pipelines/taxirio/staging/constants.py
+++ pipelines/taxirio/staging/constants.py
@@ -5,6 +5,6 @@ from enum import Enum
 class Constants(Enum):
     """Constants for the TaxiRio project."""
 
-    DATASET_ID = "brutos_taxirio_homologacao"
+    DATASET_ID = "brutos_taxirio_homologacao_staging"
     MONGODB_CONNECTION_STRING = "DB_CONNECTION_STRING_STAGING"
     RJ_TAXIRIO_AGENT_LABEL = "taxirio-staging"

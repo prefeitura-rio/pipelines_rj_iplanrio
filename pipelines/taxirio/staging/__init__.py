fab384ee45e8dd7c605bb2fc5ee13b6e1124407a
Pedro Mendes <phrmendes00@proton.me>
2025-04-03 15:40:09 -0300

    refactor(taxirio): remove staging pipelines (#49)

--- pipelines/taxirio/staging/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from pipelines.taxirio.staging.cities import *
-from pipelines.taxirio.staging.discounts import *
-from pipelines.taxirio.staging.drivers import *
-from pipelines.taxirio.staging.passengers import *
-from pipelines.taxirio.staging.paymentmethods import *
-from pipelines.taxirio.staging.races import *
-from pipelines.taxirio.staging.rankingraces import *
-from pipelines.taxirio.staging.users import *

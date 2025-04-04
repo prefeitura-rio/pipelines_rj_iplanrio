d0ff9903054b72182f198ff165bb350db16c9a25
Pedro Mendes <pedrohrmendes@proton.me>
2025-04-04 11:03:06 -0300

    fix(taxirio): remove partition per day

--- pipelines/taxirio/drivers/flows.py
+++ pipelines/taxirio/drivers/flows.py
@@ -43,7 +43,7 @@ with Flow(
         path=path,
         schema=schema,
         pipeline=pipeline,
-        partition_cols=["ano_particao", "mes_particao", "dia_particao"],
+        partition_cols=["ano_particao", "mes_particao"],
     )
 
     upload_table = create_table_and_upload_to_gcs(

72b88878a2088b251d64648c5cbd8304ee482b21
PedroSiqueira1 <pedro.siqueira@poli.ufrj.br>
2025-04-10 16:30:56 -0300

    chore: import dbt_schedules and set flow schedule

--- pipelines/dbt_transform/flows.py
+++ pipelines/dbt_transform/flows.py
@@ -11,6 +11,10 @@ from prefeitura_rio.pipelines_utils.state_handlers import handler_inject_bd_cred
 
 from pipelines.constants import Constants
 
+from pipelines.dbt_transform.schedules import (
+    dbt_schedules,
+)
+
 rj_iplanrio__dbt_transform__flow = deepcopy(templates__dbt_transform__flow)
 rj_iplanrio__dbt_transform__flow.state_handlers = [handler_inject_bd_credentials]
 rj_iplanrio__dbt_transform__flow.storage = GCS(Constants.GCS_FLOWS_BUCKET.value)
@@ -28,5 +32,7 @@ rj_iplanrio__dbt_transform__flow = set_default_parameters(
         "bigquery_project": Constants.RJ_IPLANRIO_AGENT_LABEL.value,
     },
 )
+
+rj_iplanrio__dbt_transform__flow.schedule = dbt_schedules
 
 # COMMENT TO TRIGGER.

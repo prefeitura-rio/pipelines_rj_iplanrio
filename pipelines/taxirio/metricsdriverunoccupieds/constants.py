052020eb5ec6b293fcaa616fc72f6a002a74002c
Pedro Mendes <pedrohrmendes@proton.me>
2025-04-11 12:37:21 -0300

    feat(taxirio): add metricsdriverunoccupieds collection

---
+++ pipelines/taxirio/metricsdriverunoccupieds/constants.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+
+
+class Constants(Enum):
+    """Constants for the drivers pipeline."""
+
+    TABLE_ID = "metricsdriverunoccupieds"
---
+++ pipelines/taxirio/metricsdriverunoccupieds/mongodb.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from typing import Any
+
+from pyarrow import string
+from pymongoarrow.api import Schema
+
+
+def generate_pipeline(start: datetime, end: datetime) -> list[dict[str, Any]]:
+    return [
+        {
+            "$match": {
+                "dateTime": {
+                    "$gte": start,
+                    "$lt": end,
+                },
+            },
+        },
+        {
+            "$project": {
+                "id": {"$toString": "$_id"},
+                "driver": {"$toString": "$driver"},
+                "associatedDiscount": {"$toString": "$associatedDiscount"},
+                "ano_particao": {"$dateToString": {"format": "%Y", "date": "$dateTime"}},
+                "mes_particao": {"$dateToString": {"format": "%m", "date": "$dateTime"}},
+                "dia_particao": {"$dateToString": {"format": "%d", "date": "$dateTime"}},
+            },
+        },
+        {
+            "$unset": "_id",
+        },
+    ]
+
+
+schema = Schema(
+    {
+        "id": string(),
+        "driver": string(),
+        "associatedDiscount": string(),
+        "ano_particao": string(),
+        "mes_particao": string(),
+        "dia_particao": string(),
+    },
+)
